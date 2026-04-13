27947f0a9244d5dcc694192b36bf5ce6e8ff207a
Oliver Šaravanja <oliver.saravanja@gmail.com>
2026-04-13 10:02:16 +0200

    make it clear what to upgrade with what

--- Src/Screens/HubScreen.cs
+++ Src/Screens/HubScreen.cs
@@ -255,11 +255,11 @@ public class HubScreen(GamelabGame game) : AbstractGameScreen(game)
         float dragRowY = hubPlazaBounds.Bottom - 195;
         HubShopOfferTemplate[] offers =
         [
-            new(YardStationKindIds.BasicProjectile, "Basic projectile",    18, new Color(45, 45, 50),    0f, pickupY),
+            new(YardStationKindIds.BasicProjectile, "Basic projectile",    18, new Color(255, 0, 0),    0f, pickupY),
             new(YardStationKindIds.Cannon,    "Extra cannon", 45, Color.DarkRed,          -170f, dragRowY),
             new(YardStationKindIds.Workbench,     "Extra workbench",  38, Color.DarkSlateGray,     170f, dragRowY),
-            new(YardStationKindIds.HomingCasing, "Homing casing upgrade",   50, new Color(120, 200, 120), -85f, pickupY),
-            new(YardStationKindIds.ScatterProjectile, "Scatter projectile upgrade",  25, new Color(120, 200, 120), 85f, pickupY),
+            new(YardStationKindIds.HomingCasing, "Homing casing upgrade",   50, new Color(0, 119, 255), -85f, pickupY),
+            new(YardStationKindIds.ScatterProjectile, "Scatter projectile upgrade",  25, new Color(255, 119, 0), 85f, pickupY),
         ];
 
         foreach (HubShopOfferTemplate offer in offers)
